a2761668e3951cefd8a33ed913f0f9bc6f574a46
unknown <nikhiljbabani@gmail.com>
2024-11-07 04:42:48 +0000

    feat: added flatten_data()

--- datasets/data.py
+++ datasets/data.py
@@ -9,7 +9,7 @@ def normalise_data(data):
     Normalise dataset (i.e. for pixels: [0, 255] to [0, 1])
 
     Args:
-        data (np.ndarray): Data to be normalised
+        data (np.ndarray): Dataset
 
     Returns:
         data_norm (np.ndarray): Normalised data
@@ -25,3 +25,27 @@ def normalise_data(data):
     data_norm = (data - data_max) / (data - data_min)
 
     return data_norm
+
+
+def flatten_data(data):
+    '''
+    Flatten the dataset into (28x28, m) where m is training examples
+
+    Args:
+        data (np.ndarray): Dataset
+
+    Returns:
+        data_flat (np.ndarray) = Flattened dataset
+    '''
+
+    # Obtain the number of training examples
+    training_examples = data.shape[0]
+
+    # MNIST image is square so width = height
+    image_width = data.shape[1]
+
+    # Compute total number of pixels in image
+    total_pixels = image_width**2
+
+    # Flatten the image
+    data.reshape(total_pixels, training_examples)
