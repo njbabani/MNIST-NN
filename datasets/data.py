b98d1384717b69518a6fac2935af0f39a01aadd7
unknown <nikhiljbabani@gmail.com>
2024-11-07 04:20:22 +0000

    feat: added data.py for mnist data and created normalised_data()

---
+++ datasets/data.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+Module for loading, normalising and flattening MNIST dataset
+'''
+
+
+def normalise_data(data):
+    '''
+    Normalise dataset (i.e. for pixels: [0, 255] to [0, 1])
+
+    Args:
+        data (np.ndarray): Data to be normalised
+
+    Returns:
+        data_norm (np.ndarray): Normalised data
+    '''
+
+    # Find the maximum value in data
+    data_max = data.max()
+
+    # Find the minimum value in data
+    data_min = data.min()
+
+    # Normalise the data
+    data_norm = (data - data_max) / (data - data_min)
+
+    return data_norm
