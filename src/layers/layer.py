b635919fa1575c0e76e2e2f09f8567f456f80199
unknown <nikhiljbabani@gmail.com>
2024-11-10 06:04:57 +0000

    feat!: major revision to dense and dropout layers

--- src/layers/layer.py
+++ src/layers/layer.py
@@ -6,11 +6,7 @@ This module describes the types of layers for the NN
 Classes:
     Layer: Abstract base class for layers
     Dense: Implements a fully connected (dense) layer
-
-Typical usage example:
-    dense_layer = Dense(units=64)
-    output = dense_layer(data)
-    dense_layer.update(optimiser)
+    Dropout: Implements a dropout layer for regularisation
 """
 
 from abc import ABC, abstractmethod
@@ -27,7 +23,7 @@ class Layer(ABC):
     """
     @property
     @abstractmethod
-    def output(self):
+    def layer_output(self) -> np.ndarray:
         """
         np.ndarray: Output of layer after most recent forward pass
         """
@@ -52,7 +48,8 @@ class Dense(Layer):
     Fully connected (dense) layer implementation
 
     Attributes:
-        units (int): Number of neurons in the dense layer
+        hidden_units (int): Number of neurons in the dense layer
+        input_units (int): Number of neurons in the input layer
         weights (np.ndarray): Weights matrix of the layer
         bias (np.ndarray): Bias vector of the layer
         grad_weights (np.ndarray): Gradient of weights after backpropagation
@@ -66,76 +63,76 @@ class Dense(Layer):
             hidden_units (int): Number of neurons in the dense layer
         """
         super().__init__()
-        self._hidden_units = hidden_units
-        self._input_units = None
-        self._weights = None
-        self._bias = None
-        self._output = None
-        self._dw = None
-        self._db = None
+        self.hidden_units = hidden_units
+        self.input_units = None
+        self.weights = None
+        self.bias = None
+        self.output = None
+        self.dw = None
+        self.db = None
 
     @property
-    def weights(self):
+    def layer_weights(self):
         """
         np.ndarray: The weights of the dense layer
         """
-        return self._weights
+        return self.weights
 
-    @weights.setter
-    def weights(self, weights: np.ndarray):
+    @layer_weights.setter
+    def layer_weights(self, weights: np.ndarray):
         """
         np.ndarray: Sets the weights
         """
-        self._weights = weights
+        self.weights = weights
 
     @property
     def bias(self):
         """
         np.ndarray: The bias of the dense layer
         """
-        return self._bias
+        return self.bias
 
     @bias.setter
     def bias(self, bias: np.ndarray):
         """
         np.ndarray: Sets the biases
         """
-        self._bias = bias
+        self.bias = bias
 
     @property
     def grad_weights(self):
         """
         np.ndarray: Gradient of the weights for backpropagation
         """
-        return self._dw
+        return self.dw
 
     @grad_weights.setter
     def grad_weights(self, gradients: np.ndarray):
         """
         np.ndarray: Sets the gradients of the weights
         """
-        self._dw = gradients
+        self.dw = gradients
 
     @property
     def grad_bias(self):
         """
         np.ndarray: Gradient of the biases for backpropagation
         """
-        return self._db
+        return self.db
 
     @grad_bias.setter
     def grad_bias(self, gradients: np.ndarray):
         """
         np.ndarray: Sets the gradients of the biases
         """
-        self._db = gradients
+        self.db = gradients
 
     @property
-    def output(self):
+    def layer_output(self):
         """
         np.ndarray: The output of the dense layer after forward pass
         """
-        return self._output
+        return self.output
 
     def build(self, data: np.ndarray):
         """
@@ -144,11 +141,11 @@ class Dense(Layer):
         Args:
             data (np.ndarray): Input data for determining the shape.
         """
-        self._input_units = data.shape[0]
-        random_weights = np.random.randn(self._units, self._input_units)
-        scaling_factor = np.sqrt(2.0 / self._input_units)
-        self._weights = random_weights * scaling_factor
-        self._bias = np.zeros((self._units, 1))
+        self.input_units = data.shape[0]
+        random_weights = np.random.randn(self.units, self.input_units)
+        scaling_factor = np.sqrt(2.0 / self.input_units)
+        self.weights = random_weights * scaling_factor
+        self.bias = np.zeros((self.units, 1))
 
     def __call__(self, data: np.ndarray) -> np.ndarray:
         """
@@ -160,11 +157,11 @@ class Dense(Layer):
         Returns:
             np.ndarray: Output of the layer after applying weights and biases
         """
-        if self._weights is None:
+        if self.weights is None:
             self.build(data)
 
-        self._output = np.dot(self._weights, data) + self._bias
-        return self._output
+        self.output = np.dot(self.weights, data) + self.bias
+        return self.output
 
     def update(self, optimiser: Optimiser):
         """
@@ -185,18 +182,50 @@ class Dropout(Layer):
         rate (float): Dropout rate for neurons
         mask (np.ndarray): A copy used to drop neurons during the forward prop
     """
-    def __init__(self, rate: float = 0.5):
+    def __init__(self, rate: float = 0.5, training: bool = True):
         """
         Initialises the Dropout layer
 
         Args:
             rate (float): The fraction of neurons to drop (defaults to 0.5)
+            training (bool): Specifies model is training (defaults to True)
         """
         if not 0.0 <= rate < 1.0:
             raise ValueError("Dropout rate must be in the range [0, 1).")
-        self._rate = rate
-        self._mask = None
-        self._training = True
+        self.rate = rate
+        self.mask = None
+        self.training = training
+
+    @property
+    def dropout_rate(self):
+        """
+        Obtain the dropout rate
+        """
+        return self.rate
+
+    @dropout_rate.setter
+    def dropout_rate(self, rate: float):
+        """
+        Set the dropout rate
+
+        Args:
+            rate (float): Dropout rate between [0, 1)
+        """
+        self.rate = rate
+
+    @property
+    def dropout_training_mode(self):
+        return self.training
+
+    @dropout_training_mode.setter
+    def dropout_training_mode(self, training: bool):
+        """
+        Specifies whether the model is in training
+
+        Args:
+            training (bool): If performing inference, set to False
+        """
+        self.training = training
 
     def __call__(self, data: np.ndarray) -> np.ndarray:
         """
@@ -208,22 +237,25 @@ class Dropout(Layer):
         Returns:
             np.ndarray: Output after applying dropout
         """
-        if self._training:
+        if self.training:
             # Initialise a uniform probability mask with same shape as data
             prob_mask = np.random.rand(*data.shape)
 
             # Create a mask where each element is zero with probability `rate`
-            self._mask = (prob_mask > self._rate) / (1.0 - self._rate)
+            self.mask = (prob_mask > self.rate) / (1.0 - self.rate)
 
-            # Returns element-wise product between scaled neurons and data
-            return data * self._mask
+            # Element-wise product between scaled neurons and data
+            self.output = data * self.mask
         else:
             # No dropout applied during inference
-            return data
+            self.output = data
+
+        # Return the layer output
+        return self.output
 
     @property
-    def output(self):
+    def layer_output(self):
         """
         np.ndarray: The output after the most recent forward pass
         """
-        return self._mask
+        return self.output
